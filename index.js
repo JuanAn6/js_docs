ede96485081c2c05e87178c14d95c561fae4f679
JuanAn6 <juanantoniorizija666@gmail.com>
2026-02-15 22:27:50 +0100

    Add some style and start generating the container to write text

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import { Doc } from "./src/doc.js";
+
 document.addEventListener('DOMContentLoaded', () => {
 
     let editor = document.getElementById('editor');
 
+    let config = {
+        content: 'hola',
+        editor: editor,
+    }
 
+    let doc = new Doc(config);
+
+
+    console.log('DOC', doc);
 
 
 
--- src/doc.js
+++ src/doc.js
@@ -5,7 +5,26 @@
 export class Doc {
 
     constructor(config){
+        this.element = config.editor ?? '';
         this.content = config.content ?? '';
+        this.showOptionsBar = config.showOptionsBar ?? true;
+        this.render();
+    }
+
+    render(){
+        //Default options bar...
+
+        //Editor
+        let editor = document.createElement('div');
+        editor.contentEditable = 'true';
+        editor.classList.add('editor-element');
+        
+        //generate all the elements inside the editor...
+
+        this.element.classList.add('editor-container');
+        editor.textContent = this.content;
+        this.editor = editor;
+        this.element.append(editor);
     }
 
 
